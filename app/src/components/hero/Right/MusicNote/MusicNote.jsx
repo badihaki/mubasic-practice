cbf6563ce1d39133dc1b270a1ede5a82a410519d
B. Haki Martin <jojackblack@gmail.com>
2024-04-26 01:31:43 -0400

    music note has animation and plays the audio if there is a clip supplied

--- app/src/components/hero/Right/MusicNote/MusicNote.jsx
+++ app/src/components/hero/Right/MusicNote/MusicNote.jsx
@@ -1,7 +1,19 @@
 import "./MusicNote.style.scss"
 
-export default function MusicNote({ noteText }){
-    return <div id={`note-${noteText}`} className="music-note">
+export default function MusicNote({ noteText, audioClip }){
+    function handlePlaySound(){
+        console.log("SOUND!!")
+        if(audioClip){
+            new Audio(audioClip).play();
+        }
+    }
+
+    function handleKeyboardSound(e){
+        console.log(e);
+    }
+
+
+    return <div id={`note-${noteText}`} className="music-note" onClick={handlePlaySound} onKeyDown={handleKeyboardSound} >
         <p>{noteText.toUpperCase()}</p>
     </div>
 }
