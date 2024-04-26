164d79af26465b33cea96e4bec45dccfd834723a
badihaki <jojackblack@gmail.com>
2024-04-26 02:25:01 -0400

    added audio files. Audio plays from component.

--- app/src/components/hero/Right/HeroRight.jsx
+++ app/src/components/hero/Right/HeroRight.jsx
@@ -1,8 +1,10 @@
 import "./HeroRight.Style.scss"
 import MusicNote from "./MusicNote/MusicNote"
+// audio clips below
+import chacha from "../../../assets/sounds/chacha.wav";
 
 export default function RightHero(){
     return <div id="hero__right">
-        <MusicNote noteText={`a`} />
+        <MusicNote noteText={`a`} audioClip={chacha} />
     </div>
 }
